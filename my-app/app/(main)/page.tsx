d5cb5b7d7ee3432174199762074eb87e299a6520
ines012 <celeste07102@gmail.com>
2026-04-22 10:33:21 +0800

    feat: update hero image

--- my-app/app/(main)/page.tsx
+++ my-app/app/(main)/page.tsx
@@ -1,11 +1,12 @@
+import Image from "next/image";
 import ProductCard from "./ProductCard";
 
 export default function Home() {
   return (
     <div>
       {/* Hero */}
-      <div className="h-[300px] bg-orange-200 flex items-center justify-center">
-        <h1 className="text-3xl font-bold">Tennis & Padel Gear</h1>
+      <div className="h-[350px] bg-orange-200 flex items-center justify-center relative">
+        <Image src="/hero-2.avif" alt="hero" fill className="object-cover" />
       </div>
 
       <div className="grid grid-cols-4 gap-6 p-6">
