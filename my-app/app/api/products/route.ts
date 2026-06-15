58fafa22b552f6983d6087d71882173c7f03cd17
ines012 <celeste07102@gmail.com>
2026-06-15 20:47:50 +0800

    test

--- my-app/app/api/products/route.ts
+++ my-app/app/api/products/route.ts
@@ -1,5 +1,6 @@
 import { connectDB } from "@/lib/mongodb";
 import { Product } from "@/models/Product";
+import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
