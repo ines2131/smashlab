5741e8c28d3966d04fd03df1b255afbe193a4234
ines012 <celeste07102@gmail.com>
2026-06-15 20:46:55 +0800

    test

--- my-app/app/api/products/route.ts
+++ my-app/app/api/products/route.ts
@@ -7,9 +7,17 @@ export const dynamic = "force-dynamic";
 export async function GET() {
   try {
     await connectDB();
+
+    console.log("URI:", process.env.MONGODB_URI);
+    console.log("DB:", mongoose.connection.name);
+
+    const count = await Product.countDocuments();
+
+    console.log("COUNT:", count);
+
     const products = await Product.find().lean();
 
-    return NextResponse.json(products);
+    console.log("PRODUCTS:", products);
   } catch (error) {
     return NextResponse.json(
       { message: "Failed to fetch products" },
